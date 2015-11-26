0726edc9f05edad39ef7da34b5f4c4a88f97801c
Hugo <hugo.haggmark@gmail.com>
2015-11-26 11:41:28 +0100

    cleaned up sql script and added all questions

---
+++ GradeSystem/SqlScripts/setupdb.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Questions] (
+	[Id]    INT            IDENTITY (1, 1) NOT NULL,
+	[Title] NVARCHAR (200) NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Grades] (
+	[Id]         INT      IDENTITY (1, 1) NOT NULL,
+	[Date]       DATETIME NOT NULL,
+	[QuestionId] INT      NOT NULL,
+	[Answer]     INT      NOT NULL,
+	[Week]       INT      NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC),
+
+	CONSTRAINT [FK_Grades_Questions] FOREIGN KEY ([QuestionId]) REFERENCES [dbo].[Questions] ([Id])
+);
+
+insert into [dbo].Questions (Title) values('Hur roligt har du haft det?');
+insert into [dbo].Questions (Title) values('Hur nöjd är du med egen insats/prestation?');
+insert into [dbo].Questions (Title) values('Hur nöjd är du med teamets samarbete och förmåga att arbeta tillsammans?');
+insert into [dbo].Questions (Title) values('Hur bra förutsättningar har teamet haft?');
+insert into [dbo].Questions (Title) values('Hur hög arbetsro rådde under perioden (avsaknad av störningar)?');
