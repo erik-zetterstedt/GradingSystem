0e0a76e2446c04e6a8c7b3f02a76d2e2331b96a7
Team Rocky <team.rocky@fritidsresor.se>
2016-09-29 12:24:46 +0200

    Removed avg when less than 10 values exist

--- GradeSystem/Models/Chart.cs
+++ GradeSystem/Models/Chart.cs
@@ -10,32 +10,47 @@ namespace GradeSystem.Models
 
         public IEnumerable<Dataset> Datasets { get; set; }
 
+        private bool avgExists;
+
         public void Magi()
         {
             Labels = Labels.Reverse().Take(9).Reverse();
-            Labels = new[] { "Avg last 10 weeks" }.Concat(Labels);
 
             var newDataSets = new List<Dataset>();
 
             foreach (var dataset in Datasets)
             {
-                double avg = 0;
-
                 if (dataset.Data.Count() > 9)
                 {
-                    avg = dataset
+                    AddAvgData(dataset);
+                }
+
+                newDataSets.Add(dataset);
+            }
+
+            Datasets = newDataSets;
+        }
+
+        private void AddAvgData(Dataset dataset)
+        {
+            var avg = dataset
                         .Data
                         .Reverse()
                         .Skip(9)
                         .Take(10)
                         .Average();
-                }
 
-                dataset.Data = new[] { Math.Round(avg, 1) }.Concat(dataset.Data.Reverse().Take(9).Reverse());
-                newDataSets.Add(dataset);
+            dataset.Data = new[] { Math.Round(avg, 1) }.Concat(dataset.Data.Reverse().Take(9).Reverse());
+            AddAvgLabel();
+        }
+
+        private void AddAvgLabel()
+        {
+            if (!avgExists)
+            {
+                Labels = new[] { "Avg last 10 weeks" }.Concat(Labels);
+                avgExists = true;
             }
-
-            Datasets = newDataSets;
         }
     }
 }
